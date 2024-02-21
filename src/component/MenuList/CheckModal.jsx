e9d0d910610a4be524604ad007fd3ddc2d4032d2
Agung Dwi Laksono <agungdwilaksono60@gmail.com>
2024-02-21 15:21:42 +0700

    finish project

--- src/component/MenuList/CheckModal.jsx
+++ src/component/MenuList/CheckModal.jsx
@@ -1,31 +1,82 @@
-import React from "react";
+import React, { useState } from "react";
 import "../../css/modal.css";
 import note from "../../assets/homepage/Checklist.jpg";
+import imgDeliv from "../../assets/homepage/ojek.svg";
+import { useNavigate } from "react-router-dom";
 
-const CheckModal = ({ show, handleClose, confirm }) => {
+const CheckModal = ({ show, handleClose }) => {
+  const [confirmOrder, setConfirmOrder] = useState(false);
+  const navigate = useNavigate();
+
+  const handleConfirm = () => {
+    setConfirmOrder(true);
+  };
+
   const showHideClassName = show ? "popup display-block" : "popup display-none";
   return (
     <div className={showHideClassName}>
       <div className="popup-main">
         <div className="icon-note">
-          <img style={{ width: "250px", height: "150px" }} src={note} alt="" />
+          {confirmOrder ? (
+            <img
+              style={{ width: "250px", height: "150px" }}
+              src={imgDeliv}
+              alt=""
+            />
+          ) : (
+            <img
+              style={{ width: "250px", height: "150px" }}
+              src={note}
+              alt=""
+            />
+          )}
         </div>
-        <h3>Konfirmasi Pesanan</h3>
-        <p style={{ marginTop: "15px" }}>
-          Apakah Anda yakin ingin mengkonfirmasi pesanan?
-        </p>
-        <div className="button-group">
-          <button className="close-button" onClick={handleClose}>
-            Batalkan
-          </button>
-          <button
-            style={{ backgroundColor: "rgb(10, 224, 10)" }}
-            className="confirm-button"
-            onClick={confirm}
-          >
-            Konfirmasi
-          </button>
-        </div>
+        {!confirmOrder ? (
+          <div className="sub-popup">
+            <h3>Konfirmasi Pesanan</h3>
+            <p style={{ marginTop: "15px" }}>
+              Apakah Anda yakin ingin mengkonfirmasi pesanan?
+            </p>
+            <div className="button-group">
+              <button className="close-button" onClick={handleClose}>
+                Batalkan
+              </button>
+              <button
+                style={{ backgroundColor: "rgb(10, 224, 10)" }}
+                className="confirm-button"
+                onClick={handleConfirm}
+              >
+                Konfirmasi
+              </button>
+            </div>
+          </div>
+        ) : (
+          <div className="sub-popup">
+            <h3>Terima Kasih</h3>
+            <p style={{ marginTop: "15px" }}>
+              Pesananmu akan segera diantar, Silahkan Menunggu !
+            </p>
+            <div style={{ textAlign: "center", marginTop: "30px" }}>
+              <button
+                style={{
+                  width: "50%",
+                  height: "35px",
+                  backgroundColor: "red",
+                  color: "white",
+                  borderRadius: "8px",
+                  border: "none",
+                  cursor: "pointer",
+                }}
+                onClick={() => {
+                  navigate("/");
+                }}
+              >
+                {" "}
+                Close{" "}
+              </button>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
